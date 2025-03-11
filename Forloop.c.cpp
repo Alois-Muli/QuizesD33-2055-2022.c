e382f476beafddf19060aa6281683cba1ca21dc7
Alois-Muli <aloismuli92@gmail.com>
2025-03-11 02:38:07 -0700

    Add files via upload

---
+++ Forloop.c.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Alois Muli
+Reg No: D33-2055-2022
+BMS 201
+*/
+
+#include <stdio.h>
+
+int main() {
+int i=1;
+for(i=1; i<=100; i=i+1){
+       
+printf("number is :%d, and  cube of the %d is:%d\n",i,i, (i*i*i)) ; 
+}
+return 0;
+}
