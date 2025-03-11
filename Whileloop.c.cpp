561a138574df9b5e571a51b7fe1f0437795bf029
Alois-Muli <aloismuli92@gmail.com>
2025-03-11 02:40:52 -0700

    Add files via upload

---
+++ Whileloop.c.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Alois Muli
+Reg. No: D33-2055-2022
+BMS 201
+*/
+
+#include <stdio.h>
+
+int main() {
+int i = 1; 
+while (i <=100) {
+printf("Number is: %d and cube of %d is: %d\n", i, i, i * i * i);
+i++;
+}
+
+return 0;
+}
